14535a5397162755bcf657e6935ca922e829da97
vivianjh <vivian_jh3@hotmail.com>
2016-06-05 20:43:04 +0800

    update

--- iLotus-wp/content.php
+++
@@ -1,5 +0,0 @@
-<article class="lotus-article" id="post-<?php the_ID(); ?>">
-	<h3><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( '%s', 'iLotus' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-	<?php the_content(__("", 'iLotus'));?>
-	<p class="lotus-date"><?php the_time(__('Y.m.d')); ?></p>
-</article>
--- iLotus-wp/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="lotus-footer">
-	<p>Copyright © 2010–2014 <?php bloginfo( 'name' ); ?> All rights reserved. Design by <a href="http://www.zhanxin.info" target="_blank">zhanxin</a>.</p>
-</footer>
-<script src="<?php echo get_template_directory_uri(); ?>/static/js/jquery.scrollTo.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/static/js/iLotus.js" type="text/javascript"></script>
-<?php wp_footer(); ?>
-</body>
-</html>
